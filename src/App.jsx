6a47bdcf1c2f35a7795cd5f6c5e839a3e6bab80c
William Monte de Moura <williamonte@gmail.com>
2023-04-12 16:51:17 -0300

    Estruturação do projeto em React e estruturaçao da página de login

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoginPage from './Pages/LoginPage/LoginPage';
+
+function App() {
+
+  return (
+    <div className="App">
+        <LoginPage />
+    </div>
+  )
+}
+
+export default App;
