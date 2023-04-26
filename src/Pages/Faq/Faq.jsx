bbb54575516bb238625aae42592305c3c9e19099
William Monte de Moura <williamonte@gmail.com>
2023-04-26 12:15:10 -0300

    Correção FAQ

--- src/Pages/Faq/Faq.jsx
+++ src/Pages/Faq/Faq.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import  { Accordion } from 'react-bootstrap';
 
-export const Sac = () => {
+export const Faq = () => {
     return (
         <Accordion defaultActiveKey="0">
             <Accordion.Item eventKey="0">
